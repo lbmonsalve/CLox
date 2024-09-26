a11a7c75358b9129f78e1c3362f9ca5903f102b0
Bernardo Monsalve <lbmonsalve@hotmail.com>
2024-09-25 21:39:50 -0700

    ini

--- CLox/src/vm.c
+++ CLox/src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -27,10 +28,9 @@ Value pop() {
     return *vm.stackTop;
 }
 
-InterpretResult interpret(Chunk* chunk) {
-	vm.chunk = chunk;
-	vm.ip = vm.chunk->code;
-	return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
 static InterpretResult run() {
