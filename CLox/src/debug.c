838bff0cbe7608015ffef59b2464d1f6a8f92c88
Bernardo Monsalve <lbmonsalve@hotmail.com>
2024-10-05 00:27:49 -0700

    chap19

--- CLox/src/debug.c
+++ CLox/src/debug.c
@@ -11,6 +11,20 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
+static int simpleInstruction(const char* name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+static int constantInstruction(const char* name, Chunk* chunk,
+    int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
     if (offset > 0 &&
@@ -69,18 +83,4 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         printf("Unknown opcode %d\n", instruction);
         return offset + 1;
     }
-}
-
-static int simpleInstruction(const char* name, int offset) {
-    printf("%s\n", name);
-    return offset + 1;
-}
-
-static int constantInstruction(const char* name, Chunk* chunk,
-    int offset) {
-    uint8_t constant = chunk->code[offset + 1];
-    printf("%-16s %4d '", name, constant);
-    printValue(chunk->constants.values[constant]);
-    printf("'\n");
-    return offset + 2;
 }
